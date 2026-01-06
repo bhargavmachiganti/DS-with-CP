71eedb5cbf7eaaa76aaba50eed84c3e293719e8b
bhargavmachiganti <241801120009@cutmap.ac.in>
2026-01-06 12:24:49 +0530

    Create Task4.java

---
+++ Task4.java
@@ -0,0 +1,25 @@
+Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+class Nodel int data;
+Node next;
+Node (int a){
+data = a;
+next=nu11;
+//getData
+public void getData () (
+System.out.println(data);
+}
+public void getRef ( {
+System.out.println(next);
+class Main 1
+public static void main(String[] args) {
+System. out. printIn("LOKESH");
+Node a=new Node (100);
+System.out.println(a);
+a. getData ();
+Node b=new Node (50);
+System.out.println(b);
+b. getData ();
+a. next=b;
+b. getRef() ;
+}
